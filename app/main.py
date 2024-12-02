742abafe762464ee3b54e0ee42ec988e65ddffaa
MrJuise <126503964+MrJuise@users.noreply.github.com>
2024-12-02 23:26:05 +0300

    Homework 2

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ from routes import task, user
 
 app = FastAPI()
 
-app.include_router(user.router)
-app.include_router(task.router)
-
 
 @app.get("/")
 async def welcome():
     return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(user.router)
+app.include_router(task.router)
---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+from app.models.task import Task
+from sqlalchemy.schema import CreateTable
+
+print(CreateTable(Task.__table__))
