80f964dffeb8b7675721cb188398693794e3e350
MrJuise <126503964+MrJuise@users.noreply.github.com>
2024-12-03 17:54:24 +0300

    Homework 5

--- app/routes/user.py
+++ app/routes/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from app.backend.db_depends import get_db
 # Аннотации, Модели БД и Pydantic.
 from typing import Annotated
-from app.models import User
+from app.models import User, Task
 from app.schemas import CreateUser, UpdateUser
 # Функции работы с записями.
 from sqlalchemy import insert, select, update, delete
@@ -15,7 +15,6 @@ from slugify import slugify
 
 router = APIRouter(prefix="/user", tags=["user"])
 
-DB_session = Annotated[Session, Depends(get_db)]
 
 @router.get("/")
 async def all_users(db: Annotated[Session, Depends(get_db)]):
@@ -29,6 +28,17 @@ async def user_by_id(user_id: int, db: Annotated[Session, Depends(get_db)]):
     if user is None:
         raise HTTPException(status_code=404, detail="User was not found")
     return user
+
+
+@router.get("/user_id/tasks")
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    tasks = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+    if tasks is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Tasks was not found'
+        )
+    return tasks
 
 
 @router.post("/create")
@@ -76,6 +86,7 @@ async def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
     existing_user = db.scalar(select(User).where(User.id == user_id))
     if existing_user:
         db.execute(delete(User).where(User.id == user_id))
+        db.execute(delete(Task).where(Task.user_id == user_id))
         db.commit()
         return {"status_code": status.HTTP_200_OK, "transaction": "User delete is successful!"}
     else:
